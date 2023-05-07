4588b636cffc1b57c0ba948baa58f0ecf1e7a405
VIbanichuka <94909597+VIbanichuka@users.noreply.github.com>
2023-05-07 02:40:03 +0000

    feat: CompareTheTriplets & Updated entire project!

--- src/Algorithm.Console/Program.cs
+++ src/Algorithm.Console/Program.cs
@@ -3,29 +3,40 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var c = new SwappingNumbers();
-        Console.WriteLine("Please enter the first number");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Please enter the second number");
-        int num2 = Convert.ToInt32(Console.ReadLine());
-        c.SwapTwoNumbersMethod1(num1, num2);
-        c.SwapTwoNumbersMethod2(num1, num2);
+        // var c = new SwappingNumbers();
+        // Console.WriteLine("Please enter the first number");
+        // int num1 = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Please enter the second number");
+        // int num2 = Convert.ToInt32(Console.ReadLine());
+        // c.SwapTwoNumbersMethod1(num1, num2);
+        // c.SwapTwoNumbersMethod2(num1, num2);
 
-        Console.WriteLine("");
-        Console.WriteLine("FIBONACII ALOGRITHM");
+        // Console.WriteLine("");
+        // Console.WriteLine("FIBONACII ALOGRITHM");
 
-        var fibonacci = new Fibonacci();
-        Console.WriteLine("Please enter a number");
-        int number = Convert.ToInt32(Console.ReadLine());
-        fibonacci.GenerateFibonacci(number);
+        // var fibonacci = new Fibonacci();
+        // Console.WriteLine("Please enter a number");
+        // int number = Convert.ToInt32(Console.ReadLine());
+        // fibonacci.GenerateFibonacci(number);
+
+        // var time = new TimeConversion();
+        // var hour24 = time.ConvertTo24hour("11:30:45PM");
+        // Console.WriteLine(hour24);
+
+        // SumOfArray sum = new SumOfArray();
+        // var numbers = new List<int>() {1,2,3,8};
+        // var totalNumber = sum.SimpleArraySum(numbers);
+        // System.Console.WriteLine(totalNumber);
+
+
+        var compare = new CompareTheTriplets();
+        var a = new List<int>() { 17, 28, 30 };
+        var b = new List<int>() { 99, 16, 8 };
+        var result = compare.CompareTriplets(a, b);
+        for (int i = 0; i < result.Count; i++)
+        {
+            System.Console.WriteLine(result[i]);
+        }
         
-        var time = new TimeConversion();
-        var hour24 = time.ConvertTo24hour("11:30:45PM");
-        Console.WriteLine(hour24);
-
-        SumOfArray sum = new SumOfArray();
-        var numbers = new List<int>() {1,2,3,8};
-        var totalNumber = sum.SimpleArraySum(numbers);
-        System.Console.WriteLine(totalNumber);
     }
 }
--- src/Algorithms/TimeConversion.cs
+++ src/Algorithms/TimeConversion.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace C__Algorithm;
 public class TimeConversion
 {
